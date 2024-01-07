2e12618a09bceb7572ae79d6bf16c33b1b2cb5fb
Karthikeyan S <121245383+kuttykarthikeyan@users.noreply.github.com>
2024-01-07 22:54:00 +0530

    Update InfixToPostfix.c

--- InfixToPostfix.c
+++ InfixToPostfix.c
@@ -1,63 +1,80 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <ctype.h>
+#include <conio.h>
 #include <string.h>
-
 char stack[10];
-int top = -1;
+int size = 10;
+int top=-1;
 
-void push(char x) {
-    stack[++top] = x;
-}
-
-char pop() {
-    return stack[top--];
-}
-
-int preced(char x) {
-    if (x == '(')
-        return 0;
-    else if (x == '+' || x == '-')
+int is_full()
+{
+    if(top==(size-1))
         return 1;
-    else if (x == '*' || x == '/')
-        return 2;
-
+    else
+        return 0;
 }
 
-int main() {
-    char array[10];
-    printf("Enter infix expression: ");
-    scanf("%s", array);
+int is_empty()
+{
+    if(top==-1)
+        return 1;
+    else
+        return 0;
+}
 
+void push(char temp)
+{
+    stack[++top]=temp;
+}
 
-    int i = 0;char j;
-    while (array[i] != '\0') {
-        if (isalnum(array[i])) {
-            printf("%c",array[i]);
-        } else if (array[i] == '(') {
-            push(array[i]);
-        } else if (array[i] == ')') {
-            while (j = pop()) {
-                    if(j=='(')
-                       break;
-                    else
-                    printf("%c",j);
+char pop()
+{
+    char elem = stack[top];
+    stack[top] = 0;
+    top--;
+    return elem;
+}
+
+int preced(char temp)
+{
+    if(temp=='(')
+        return 0;
+    else if(temp=='+' || temp=='-')
+        return 1;
+    else if(temp=='*' || temp=='/')
+        return 2;
+}
+
+void main()
+{
+    char exp[20];
+    printf("Enter the infix expression : ");
+    scanf("%s",exp);
+    int i = 0;
+    while(exp[i]!='\0')
+    {
+        if(isalnum(exp[i]))
+            printf("%c",exp[i]);
+        else if(exp[i]=='(')
+            push(exp[i]);
+        else if(exp[i]==')')
+        {
+            char m;
+            while(m=pop())
+            {
+                if(m=='(')
+                   break;
+                printf("%c",m);
             }
         }
-        else {
-            while (top >= 0 && preced(stack[top]) >= preced(array[i])) {
+        else
+        {
+            if(preced(stack[top]) >= preced(exp[i]))
                 printf("%c",pop());
-            }
-            push(array[i]);
+            push(exp[i]);
         }
         i++;
     }
-
-    while (top >= 0) {
+    while(top!=-1)
         printf("%c",pop());
-    }
-
-
-
-    return 0;
 }
