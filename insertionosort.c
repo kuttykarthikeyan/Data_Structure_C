f842d2e7bc3b6a94f1bac872e5728c13d0f7d70e
Karthikeyan S <121245383+kuttykarthikeyan@users.noreply.github.com>
2024-01-07 21:19:25 +0530

    Create insertionosort.c

---
+++ insertionosort.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+void insertionSort(int arr[], int n) {
+    int i, temp, j;
+
+    for (i = 1; i < n; i++) {
+        temp = arr[i];
+        j = i - 1;
+
+        // Move elements of arr[0..i-1] that are greater than key to one position ahead of their current position
+        while (j >= 0 && arr[j] > temp) {
+            arr[j + 1] = arr[j];
+            j--;  // Fix: Remove the unnecessary assignment here
+        }
+        arr[j + 1] = temp;
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: ");
+    printArray(arr, n);
+
+    insertionSort(arr, n);
+
+    printf("Sorted array: ");
+    printArray(arr, n);
+
+    return 0;
+}
