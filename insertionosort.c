82b60f24ecad4c52c5b5e9a524a4f8fba0baefbb
Karthikeyan S <121245383+kuttykarthikeyan@users.noreply.github.com>
2024-01-07 21:20:23 +0530

    Update insertionosort.c

--- insertionosort.c
+++ insertionosort.c
@@ -7,10 +7,9 @@ void insertionSort(int arr[], int n) {
         temp = arr[i];
         j = i - 1;
 
-        // Move elements of arr[0..i-1] that are greater than key to one position ahead of their current position
         while (j >= 0 && arr[j] > temp) {
             arr[j + 1] = arr[j];
-            j--;  // Fix: Remove the unnecessary assignment here
+            j--;  
         }
         arr[j + 1] = temp;
     }
