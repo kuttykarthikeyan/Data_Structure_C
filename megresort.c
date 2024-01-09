70cd3cc27ece12d67a80ff6baeaf081577a9c85c
Karthikeyan S <121245383+kuttykarthikeyan@users.noreply.github.com>
2024-01-09 21:58:33 +0530

    Update megresort.c

--- megresort.c
+++ megresort.c
@@ -2,48 +2,34 @@
 #include <stdlib.h>
 
 void merge(int arr[], int left, int mid, int right) {
-    int i, j, k;
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
-
-    // Create temporary arrays
-    int L[n1], R[n2];
-
-    // Copy data to temporary arrays L[] and R[]
-    for (i = 0; i < n1; i++)
-        L[i] = arr[left + i];
-    for (j = 0; j < n2; j++)
-        R[j] = arr[mid + 1 + j];
-
-    // Merge the temporary arrays back into arr[left..right]
-    i = 0;
-    j = 0;
-    k = left;
-
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
-            i++;
-        } else {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
-    }
-
-    // Copy the remaining elements of L[], if there are any
-    while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
-
-    // Copy the remaining elements of R[], if there are any
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
+  int i,j;
+  i=left;
+  j=mid+1;
+  int p=0;
+  int b[(right-left)+1];
+  while(i<=mid&&j<=right){
+      if(arr[i]<arr[j]){
+        b[p++]=arr[i];    
+          i++;
+      }
+      else{
+          b[p++]=arr[j];
+          j++;
+      }
+  }
+  while(i<=mid){
+      b[p++]=arr[i];
+      i++;
+  }
+  while(j<=right){
+      b[p++]=arr[j];
+      j++;
+  }
+  int k=0;
+  for(int i=left;i<=right;i++){
+      arr[i]=b[k];
+      k++;
+  }
 }
 
 void mergeSort(int arr[], int left, int right) {
